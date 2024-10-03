6bc8df27b832ed0808c7511873591a7f2d4b9c35
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-10-03 06:49:57 -0700

    task 0

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""task 0"""
+
+
+def sieve_of_eratosthenes(n):
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False  # 0 and 1 are not prime numbers
+    for i in range(2, int(n ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = False
+    return sieve
+
+
+def isWinner(x, nums):
+    if not nums or x < 1:
+        return None
+    max_num = max(nums)
+    prime = sieve_of_eratosthenes(max_num)
+    maria_wins = 0
+    ben_wins = 0
+    for n in nums:
+        primes_count = sum(prime[2:n+1])
+        if primes_count % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
